4ec55acddf1abdda963600978d9a06863b0c5e4d
egrinnn <m-altaiskaia@mail.ru>
2026-02-26 03:28:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ fake = Faker()
 
 app = Flask(__name__)
 application = app
+templates_folder = '.'
 
 images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
               '2d2ab7df-cdbc-48a8-a936-35bba702def5',
